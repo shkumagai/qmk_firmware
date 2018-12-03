c8f9137a4a9b8a08735ba1eeb2f5deff1413afdf
shkumagai <take.this.2.your.grave@gmail.com>
2018-12-04 08:58:07 +0900

    Add shkumagai_keymap

---
+++ keyboards/iris/keymaps/shkumagai/keymap.c
@@ -0,0 +1,133 @@
+#include QMK_KEYBOARD_H
+
+extern keymap_config_t keymap_config;
+
+#define _QWERTY 0
+#define _LOWER 1
+#define _RAISE 2
+#define _ADJUST 16
+
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  LOWER,
+  RAISE,
+  ADJUST,
+};
+
+#define KC_ KC_TRNS
+
+#define KC_LOWR LOWER
+#define KC_RASE RAISE
+
+#define KC_RST RESET
+#define KC_RTOG RGB_TOG
+#define KC_RFWD RGB_MOD
+#define KC_RREW RGB_RMOD
+#define KC_RHUI RGB_HUI
+#define KC_RHUD RGB_HUD
+#define KC_RSAI RGB_SAI
+#define KC_RSAD RGB_SAD
+#define KC_RVAI RGB_VAI
+#define KC_RVAD RGB_VAD
+#define KC_BTOG BL_TOGG
+#define KC_BLVL BL_STEP
+#define KC_BRTG BL_BRTG
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [_QWERTY] = LAYOUT_kc(
+  //+-----+-----+-----+-----+-----+-----+                 +-----+-----+-----+-----+-----+-----+
+      GRV ,  1  ,  2  ,  3  ,  4  ,  5  ,                    6  ,  7  ,  8  ,  9  ,  0  , BSPC,
+  //+-----+-----+-----+-----+-----+-----+                 +-----+-----+-----+-----+-----+-----+
+      TAB ,  Q  ,  W  ,  E  ,  R  ,  T  ,                    Y  ,  U  ,  I  ,  O  ,  P  , BSLS,
+  //+-----+-----+-----+-----+-----+-----+                 +-----+-----+-----+-----+-----+-----+
+      LCTL,  A  ,  S  ,  D  ,  F  ,  G  ,                    H  ,  J  ,  K  ,  L  , SCLN, QUOT,
+  //+-----+-----+-----+-----+-----+-----+-----+     +-----+-----+-----+-----+-----+-----+-----+
+      LSFT,  Z  ,  X  ,  C  ,  V  ,  B  ,     ,       RCTL,  N  ,  M  , COMM, DOT , SLSH, RSFT,
+  //+-----+-----+-----+-----+-----+-----+-----+     +-----+-----+-----+-----+-----+-----+-----+
+                              LGUI, LOWR, SPC ,       ENT , RASE, LALT
+                          //+-----+-----+-----+     +-----+-----+-----+
+  ),
+
+  [_LOWER] = LAYOUT_kc(
+  //+-----+-----+-----+-----+-----+-----+                 +-----+-----+-----+-----+-----+-----+
+      ESC , EXLM, AT  , HASH, DLR , PERC,                   CIRC, AMPR, ASTR, LPRN, RPRN, BSPC,
+  //+-----+-----+-----+-----+-----+-----+                 +-----+-----+-----+-----+-----+-----+
+      GRV ,     ,     ,     ,     , LBRC,                   RBRC,     ,     ,     ,     ,     ,
+  //+-----+-----+-----+-----+-----+-----+                 +-----+-----+-----+-----+-----+-----+
+          ,     ,     ,     ,     , LCBR,                   RCBR,     , UP  ,     , PLUS, PIPE,
+  //+-----+-----+-----+-----+-----+-----+-----+     +-----+-----+-----+-----+-----+-----+-----+
+          ,     ,     ,     ,     , LPRN,     ,           , RPRN, LEFT, DOWN, RGHT, MINS,     ,
+  //+-----+-----+-----+-----+-----+-----+-----+     +-----+-----+-----+-----+-----+-----+-----+
+                                  ,     , DEL ,       BSPC,     ,
+                          //+-----+-----+-----+     +-----+-----+-----+
+  ),
+
+  [_RAISE] = LAYOUT_kc(
+  //+-----+-----+-----+-----+-----+-----+                 +-----+-----+-----+-----+-----+-----+
+      F12 , F1  , F2  , F3  , F4  , F5  ,                   F6  , F7  , F8  , F9  , F10 , F11 ,
+  //+-----+-----+-----+-----+-----+-----+                 +-----+-----+-----+-----+-----+-----+
+          , EXLM, AT  , HASH, DLR , PERC,                   CIRC, AMPR, ASTR, LPRN, RPRN,     ,
+  //+-----+-----+-----+-----+-----+-----+                 +-----+-----+-----+-----+-----+-----+
+          ,     ,     , VOLU, PGUP, UNDS,                   EQL ,     ,     ,     ,     , BSLS,
+  //+-----+-----+-----+-----+-----+-----+-----+     +-----+-----+-----+-----+-----+-----+-----+
+      MUTE,     ,     , VOLD, PGDN, MINS, LPRN,           , PLUS,     ,     ,     ,     ,     ,
+  //+-----+-----+-----+-----+-----+-----+-----+     +-----+-----+-----+-----+-----+-----+-----+
+                                  ,     ,     ,           ,     ,
+                          //+-----+-----+-----+     +-----+-----+-----+
+  ),
+
+  [_ADJUST] = LAYOUT_kc(
+  //+-----+-----+-----+-----+-----+-----+                 +-----+-----+-----+-----+-----+-----+
+          ,     ,     ,     ,     ,     ,                       ,     ,     ,     ,     ,     ,
+  //+-----+-----+-----+-----+-----+-----+                 +-----+-----+-----+-----+-----+-----+
+      RTOG, RFWD, RHUI, RSAI, RVAI,     ,                       ,     ,     ,     ,     ,     ,
+  //+-----+-----+-----+-----+-----+-----+                 +-----+-----+-----+-----+-----+-----+
+          , RREW, RHUD, RSAD, RVAD,     ,                       ,     ,     ,     ,     ,     ,
+  //+-----+-----+-----+-----+-----+-----+-----+     +-----+-----+-----+-----+-----+-----+-----+
+          ,     ,     ,     ,     ,     ,     ,           ,     ,     ,     ,     ,     ,     ,
+  //+-----+-----+-----+-----+-----+-----+-----+     +-----+-----+-----+-----+-----+-----+-----+
+                                  ,     ,     ,           ,     ,
+                          //+-----+-----+-----+     +-----+-----+-----+
+  )
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(_QWERTY);
+      }
+      return false;
+      break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case ADJUST:
+      if (record->event.pressed) {
+        layer_on(_ADJUST);
+      } else {
+        layer_off(_ADJUST);
+      }
+      return false;
+      break;
+  }
+  return true;
+}
